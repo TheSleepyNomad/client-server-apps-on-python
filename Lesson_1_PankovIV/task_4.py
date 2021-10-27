b531be952dee35bb359cbd8dc38050c2e12531d5
TheSleepyNomad <iluxaan@mail.ru>
2021-10-27 10:45:50 +0300

    feat: complite and add solution for task #4

--- Lesson_1_PankovIV/task_4.py
+++ Lesson_1_PankovIV/task_4.py
@@ -8,3 +8,12 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+words_set = ('разработка', 'администрирование', 'protocol', 'standard')
+b_words_set = []
+
+for byte_str in words_set:
+    b_words_set.append(byte_str.encode('utf-8'))
+print(f'{b_words_set}\n')
+
+for string in b_words_set:
+    print(string.decode('utf-8'))
