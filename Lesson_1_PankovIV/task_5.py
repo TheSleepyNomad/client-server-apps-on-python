d748caa186d59ed3266e171f433cc0482eaaa79d
TheSleepyNomad <iluxaan@mail.ru>
2021-10-27 10:52:33 +0300

    feat: complite and add solution for task #5

--- Lesson_1_PankovIV/task_5.py
+++ Lesson_1_PankovIV/task_5.py
@@ -7,3 +7,14 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+import subprocess
+import chardet
+
+ping_set = (subprocess.Popen(['ping', 'google.com'], stdout=subprocess.PIPE),
+            subprocess.Popen(['ping', 'yandex.ru'], stdout=subprocess.PIPE),
+            subprocess.Popen(['ping', 'youtube.com'], stdout=subprocess.PIPE),)
+
+for subproc in ping_set:
+    for line in subproc.stdout:
+        line = line.decode(chardet.detect(line).get('encoding')).encode('utf-8')
+        print(line.decode('utf-8'))
