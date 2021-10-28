4da8cf56bae019625356515234cc881b8dab5781
TheSleepyNomad <iluxaan@mail.ru>
2021-10-28 15:38:34 +0300

    feat: complite task_1 for lesson_2

--- Lesson_2_PankovIV/task_1.py
+++ Lesson_2_PankovIV/task_1.py
@@ -25,3 +25,56 @@
 """
 
 import csv
+from typing import Union
+import re
+import os.path
+
+# список файлов с данными
+data_files = ('info_1.txt', 'info_2.txt', 'info_3.txt')
+path_to_csv = r'./test.csv'
+
+
+def get_data(data: Union[str, list, tuple]):
+    main_data = [['Изготовитель системы',
+                 'Название ОС',
+                  'Код продукта',
+                  'Тип системы']]
+    os_prod_list, os_name_list, os_code_list, os_type_list = [], [], [], []
+
+    if isinstance(data, list) or isinstance(data, tuple):
+        for file_name in data:
+            with open(file_name, 'rb') as f_data:
+                for row in f_data.read().decode('cp1251').splitlines():
+                    os_prod = re.search(r'Изготовитель системы', row)
+                    os_name = re.search(r'Название ОС', row)
+                    os_code = re.search(r'Код продукта', row)
+                    os_type = re.search(r'Тип системы', row)
+                    data_row = row.split(':')
+                    if os_prod:
+                        os_prod_list.append(data_row[1].strip())
+                    if os_name:
+                        os_name_list.append(data_row[1].strip())
+                    if os_code:
+                        os_code_list.append(data_row[1].strip())
+                    if os_type:
+                        os_type_list.append(data_row[1].strip())
+
+        for row in range(len(os_prod_list)):
+            main_data.append([os_prod_list[row],
+                              os_name_list[row],
+                              os_code_list[row],
+                              os_type_list[row]])
+
+        return main_data
+
+
+def write_to_csv(path_to_csv: str):
+    if os.path.exists(path_to_csv):
+        with open(path_to_csv, 'w+', encoding='utf-8') as file_csv:
+            data_for_csv = get_data(data_files)
+            csv_writer = csv.writer(file_csv)
+            for row in data_for_csv:
+                csv_writer.writerow(row)
+
+
+write_to_csv(path_to_csv)
